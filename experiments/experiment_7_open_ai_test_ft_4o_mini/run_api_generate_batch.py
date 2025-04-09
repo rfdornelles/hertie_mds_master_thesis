5c7c9d5035b77c8ad9f7d79b572f8d2daefc2301
Rodrigo Dornelles <rodornelles@gmail.com>
2025-04-09 18:29:10 +0200

    experiment 7 with ft data

---
+++ experiments/experiment_7_open_ai_test_ft_4o_mini/run_api_generate_batch.py
@@ -0,0 +1,263 @@
+## objective: run trough the OpenAI api the validation samples
+# using the chepeast model available (4o-mini)
+
+# reference: https://cookbook.openai.com/examples/batch_processing
+
+import dotenv
+import pandas as pd
+import os
+from openai import OpenAI
+import json
+from datasets import load_from_disk
+import time
+from datetime import datetime
+
+
+#### load environment variables from .env file
+dotenv.load_dotenv()
+
+API_KEY = os.getenv("HERTIE_OPENAI_API_KEY")
+HERTIE_OPENAI_ORG_ID = os.getenv("HERTIE_OPENAI_ORG_ID")
+HERTIE_OPENAI_PROJECT_ID = os.getenv("HERTIE_OPENAI_PROJECT_ID")
+
+#### definitions
+model = 'ft:gpt-4o-mini-2024-07-18:hertie-school:judicial-sentences:BKQ48gf0'
+experiment = os.path.basename(os.path.abspath(os.path.dirname(__file__)))
+dataset = "test"
+prompt = "prompt_v4_clean.md"
+schema = "schema.json"
+
+if model.startswith("o"):
+  print(f"[ATTENTION] Using the OpenAI model {model} - this model doesnt' support max tokens")
+
+print(f"Running experiment: {experiment}")
+print(f"Using model: {model}")
+print(f"Using dataset: {dataset}")
+print(f"Using prompt: {prompt}")
+print(f"Using schema: {schema}")
+#### download data
+df = load_from_disk(f"../../data/{dataset}").to_pandas()
+
+if (model.startswith("ft:")):
+  ## using the test + validation sets
+  print(f"[ATTENTION] Since using a ft model ({model}) we are using the test + validation sets")
+  df = pd.concat(
+    [
+     load_from_disk(f"../../data/test").to_pandas(),
+     load_from_disk(f"../../data/validation").to_pandas()
+    ]
+  ) 
+else:
+  df = load_from_disk(f"../../data/{dataset}").to_pandas()
+
+
+  
+### load prompt
+with open(f"../../prompt/{prompt}", "r") as f:
+    prompt = f.read()
+    
+### load schema
+schema = json.load(open(f"../../prompt/{schema}", "r"))
+
+### build the client
+client = OpenAI(
+  api_key=API_KEY,
+  organization=HERTIE_OPENAI_ORG_ID,
+  project=HERTIE_OPENAI_PROJECT_ID,
+  )
+
+## generate the batch
+# check if the json file already exists
+if os.path.exists(f"{experiment}-batch.jsonl"):
+    print(f"Batch file {experiment}-batch.jsonl already exists.")
+  
+else:
+  print(f"Batch file {experiment}-batch.jsonl does not exist. Generating a new one.")
+  tasks = []
+  
+  # if the model name os `o_xxx' the parameter is called max_completion_tokens
+  max_tokens_name = "max_completion_tokens" if model.startswith("o") else "max_tokens"
+                                                            
+  max_tokens_value = 3000 if model.startswith("o") else 1000
+  
+  for index, row in df.iterrows():
+    
+      description = f"{experiment}:{row['processo']}:{json.loads(row['output'])['nome']}"
+      body = {
+          "model": model,
+          #max_tokens_name: max_tokens_value,
+          "messages": [
+          { "role": "system", "content": f"{prompt}\n\n" +  f"O número do processo deve ser: {json.dumps(row['processo'])}" },
+          
+          { "role": "user", "content": f"SENTENÇA/ATA A SER ANALISADA: {json.dumps(row['input'])}" }
+          ],
+          "response_format": { 
+          "type": "json_schema", 
+          "json_schema": schema 
+          }
+      }
+      
+      if not model.startswith("o"):
+          body["temperature"] = 0
+          body[max_tokens_name] = max_tokens_value
+
+      task = {
+          "custom_id": description,
+          "method": "POST",
+          "url": "/v1/chat/completions",
+          "body": body
+      }
+      tasks.append(task)
+      
+  ## generate the .jsonl file
+  file_name = f"{experiment}-batch.jsonl"
+  with open(file_name, "w", encoding="utf-8") as f:
+      for task in tasks:
+          f.write(json.dumps(task, ensure_ascii=True) + "\n")
+  print(f"Batch file {file_name} generated with {len(tasks)} tasks.")
+
+  ## upload the batch
+  batch_file = client.files.create(
+      file=open(file_name, "rb"),
+      purpose="batch"
+  )
+  print(f"Batch file {batch_file.id} uploaded.")
+
+  ## create the batch job
+
+  batch_job = client.batches.create(
+    input_file_id= batch_file.id,
+    endpoint="/v1/chat/completions",
+    completion_window='24h'
+  )
+
+  # save the batch job id
+  with open(f"{experiment}-batch_job_id.txt", "w") as f:
+      f.write(batch_job.id)
+  print(f"Batch job {batch_job.id} created.")
+
+# get the batch job id
+with open(f"{experiment}-batch_job_id.txt", "r") as f:
+    batch_job_id = f.read()
+
+## check status
+def get_batch_job_status(batch_job_id):
+    batch_job_status = client.batches.retrieve(batch_job_id)
+    
+    if(batch_job_status.status == "validating"):
+      print("Batch job is validating...")
+      return batch_job_status, 0.1
+    
+    if (batch_job_status.status == "finalizing"):
+        print(f"Batch job is finalizing: {batch_job_status.status}")
+        return batch_job_status, 0.01
+          
+    elif (batch_job_status.status == "processing"):
+        print(f"Batch job is still processing: {batch_job_status.status}")
+        return batch_job_status, 0.5
+    elif (batch_job_status.status == "in_progress"):
+        print(f"Batch job is in progress: {batch_job_status.status}")
+        return batch_job_status, 5
+    elif (batch_job_status.status == "completed"):
+        print(f"Batch job completed: {batch_job_status.status}")
+        # download the results
+        print(f"Completed!!")
+        print(f"Completed at: {datetime.fromtimestamp(batch_job_status.completed_at)}")
+        return batch_job_status, 0
+    elif (batch_job_status.status == "failed"):
+        print(f"Batch job failed: {batch_job_status.status}")
+
+    return batch_job_status, 0
+
+status, wait_time = get_batch_job_status(batch_job_id) 
+
+while status.status not in ["completed", "failed"]:
+    print("Waiting for batch job to complete...")
+    print(f"Status: {status.status}")
+    print(f"Job started at: {datetime.fromtimestamp(status.created_at)}")
+    print(f"Job expiration: {datetime.fromtimestamp(status.expires_at)}")
+  
+    time.sleep(wait_time)  # wait for the specified time before checking again
+    status, wait_time = get_batch_job_status(batch_job_id)  # update status and wait_time
+    
+    total = status.request_counts.total
+    completed = status.request_counts.completed
+    failed = status.request_counts.failed
+    
+    if total > 0:
+        print(f"Total requests: {total} - Completed: {completed} ({completed/total*100:.2f}%) - Failed: {failed} ({failed/total*100:.2f}%)")
+    else:
+        print(f"Total requests: {total} - Completed: {completed} - Failed: {failed}")
+
+ 
+    
+    time.sleep(60*wait_time) # wait for 5 minutes
+    status, wait_time = get_batch_job_status(batch_job_id)
+
+output_path = f"{experiment}-batch_results.jsonl"
+
+if os.path.exists(output_path):
+    print(f"Batch results file {output_path} already exists.")
+    
+else:
+    print(f"Batch results file {output_path} does not exist. Generating a new one.")
+    # download the results
+    result_file_id = status.output_file_id
+    result = client.files.content(result_file_id).content
+    
+    # write the result to a file
+    try: 
+      with open(output_path, "wb") as f:
+        f.write(result)
+    except Exception as e:
+        print(f"Error writing file: {e}")
+        exit(1)
+  
+    print(f"Batch job metadata saved to {output_path}")
+
+errors = []
+
+## open the results
+with open(output_path, "r", encoding="utf-8") as f:
+    lines = f.readlines()
+    results = []
+    for line in lines:
+        try:
+            result = json.loads(line)
+            results.append(result)
+        except Exception as e:
+            print(f"Error parsing line: {e}")
+            print(line)
+            errors.append(line)
+            continue
+
+    df_results = pd.DataFrame(results)
+    df_results.to_csv(f"{experiment}-batch_metadata_results.csv", index=False)
+    print(f"Batch job results saved to {experiment}-batch_results.csv")
+
+## process the results
+
+df_rows = []
+for index, row in df_results.iterrows():
+  try:
+    # load the content from the response
+    content = json.loads(row['response']['body']['choices'][0]['message']['content'])
+    # capture custom_id and set it as the first key in a new dictionary
+    custom_id = row.get('custom_id', None)
+    new_content = {'custom_id': custom_id}
+    # merge the rest of the content so that custom_id remains the first column
+    new_content.update(content)
+    df_rows.append(new_content)
+    
+  except Exception as e:
+    print(f"Error parsing line: {e}")
+    print(row)
+    continue
+df_rows = pd.DataFrame(df_rows)
+
+df_rows
+
+## save
+df_rows.to_parquet(f"{experiment}-batch_results_parsed.parquet", index=False, )
+
+print(f"Batch job results parsed and saved to {experiment}-batch_results_parsed.parquet")
