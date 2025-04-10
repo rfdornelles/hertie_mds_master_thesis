de1ca15d23cfe2b5b3c0ca024a7551a1ab69b670
rfdornelles <rodornelles@gmail.com>
2025-04-10 23:34:23 +0200

    new experiments

---
+++ llama_3.2_ft/gemma3_12b_run.py
@@ -0,0 +1,141 @@
+import torch
+from transformers import AutoTokenizer, Gemma3ForCausalLM
+from datasets import load_from_disk, concatenate_datasets
+import os
+import tqdm
+
+# clean cuda just in case
+torch.cuda.empty_cache()
+
+# definitions
+experiment = 'experiment_gemma3_12b_it_baseline'
+folder = f'experiments/{experiment}/'
+model_name = "google/gemma-3-12b-it"
+
+os.makedirs(folder, exist_ok=True)
+
+
+
+# load datasets
+validation = load_from_disk('../data/validation')
+test = load_from_disk('../data/test')
+
+# merge both
+datasets = concatenate_datasets([validation, test]).to_pandas()
+
+# load prompt
+with open('../prompt/prompt_v4.md', 'r') as f:
+    prompt = f.read()
+
+## load model
+
+model = Gemma3ForCausalLM.from_pretrained(
+    model_name, 
+    torch_dtype=torch.bfloat16, 
+    # device_map="cuda",
+    device_map="auto"
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+
+## load model
+print("Model and datasets loaded!!")
+
+## function to build the prompt
+def build_prompt(input):
+  messages = [
+        [
+            {
+                "role": "system",
+                "content": [{"type": "text", "text": prompt},]
+            },
+            {
+                "role": "user",
+                "content": [{"type": "text", "text": str(input)},]
+            },
+        ],
+    ]
+    
+  return messages
+
+
+## function to iterate
+def run_model(processo, new_tokens=1000, overwrite = False):
+  
+  # clean: remove everything that's not numbers
+  processo = str(processo).replace(r'[^0-9]', '')
+  
+  # check if the file already exists
+  file = f'{folder}/{processo}.json'
+  if os.path.exists(file) and not overwrite:
+    print(f"File {file} already exists. Skipping...")
+    return
+
+  # retrieve the content of the sentença
+  sentenca = datasets[datasets['processo'] == processo]['input'].values[0]
+  
+  # build the prompt
+  message = build_prompt(sentenca)
+  
+  # run the model
+  try:
+    
+    inputs = tokenizer.apply_chat_template(
+        message, 
+        add_generation_prompt=True, 
+        tokenize=True,
+        return_dict=True, 
+        return_tensors="pt"
+    ).to(model.device)
+    
+    input_len = inputs["input_ids"].shape[-1]
+    
+    generation = model.generate(**inputs, 
+                                max_new_tokens=new_tokens, do_sample=False,
+                                temperature=None,
+                                top_p=None,
+                                top_k=None)
+  
+    # only keep the new tokens
+    generation = generation[0][input_len:]
+  
+    decoded = tokenizer.decode(generation, skip_special_tokens=True)
+    
+  except Exception as e:
+    print(f"Error processing {processo}: {e}")
+    return
+    
+    
+  # save the output as json
+  
+  try:
+    with open(file, 'w') as f:  
+      f.write(decoded)
+      return True
+    
+  except Exception as e:
+    print(f"Error writing file {file}: {e}")
+    return  
+  
+# test    
+#run_model('00316684320178260050', overwrite = True)
+
+# with open(f'{folder}/00316684320178260050.json', 'r') as f:
+#     output = f.read()
+ 
+## iterate
+print("Starting the iteration...")
+
+for processo in tqdm.tqdm(datasets['processo'].unique()):
+  try:
+    run_model(processo, overwrite = True)
+  except Exception as e:
+    print(f"Error processing {processo}: {e}")
+    continue
+  finally:
+    torch.cuda.empty_cache()
+    #print(f"Process {processo} finished.")
+    
+print("All processes finished.")   
+
+    
